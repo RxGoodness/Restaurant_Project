10445645d778728a88e6939d897607bdb32517bc
rxgoodness <adedokuntoyyib@gmail.com>
2022-05-29 14:17:51 +0100

    new commit 1

---
+++ src/store/CartProvider.js
@@ -0,0 +1,19 @@
+import CartContext from "./card-context";
+
+const CartProvider = props => {
+addItemToCartHandler = item =>{
+
+}
+const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: ,
+    removeItem:
+}
+
+    return <CartContext.Provider>
+        {props.children}
+    </CartContext.Provider>
+}
+
+export default CartProvider;
