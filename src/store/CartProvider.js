4d2d063216edc0b6ed2176b6b8626f789b8f0d74
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-15 20:48:00 +0100

    progress

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -59,7 +59,21 @@ const cartReducer = (state, action) => {
        (item)=>item.id === action.item.id
      );
      const existingItem = state.items[existingCartItemIndex]
-     const updatedTotalAmount = state.totalAmount - existingItem.price
+     const updatedTotalAmount = state.totalAmount - existingItem.price;
+     let updatedItems; 
+     if (existingItem.amount === 1){
+updatedItems = state.items.filter(item => item.id !==action.id)
+     }
+     else {
+     const updatedItem = {...existingItem, amount: existingItem.amount -1}
+     updatedItems = [...state.items];
+     updatedItems[existingCartItemIndex] = updatedItem;
+
+     }
+     return {
+       items: updatedItems,
+       totalAmount: updatedTotalAmount
+     }
    }
   return defaultCartState;
 };
